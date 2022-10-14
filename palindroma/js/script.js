eb85560f82ac756a0c3fc2e46caefa71f98fa2c9
scurenza <fedechiar@hotmail.it>
2022-10-14 15:54:41 +0200

    problemi con function

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -5,15 +5,35 @@
 // Prendere un input
 let userWord = [];
 userWord = prompt ("Scrivi una parola e ti dirò se è o no palindroma");
-console.log(userWord, typeof(userWord));
-let lessLength = userWord.length - 1;
-console.log(lessLength);
+
+if (wordIsAPalindrome(userWord)) {
+    console.log("La parola è palindroma!");
+} else {
+    console.log("La parola non è palindroma :(");
+}
+
+
 
 // Comparo le lettere della stringa con un ciclo for
-for (let i = 0; i <userWord.length; i++) {
-    if (userWord[i] === userWord[lessLength - i]) {
-        console.log("é palindroma!");
-    } else {
-        console.log(("non è palindroma"));
+
+// for (let i = 0; i <userWord.length; i++) {
+//     if (userWord[i] === userWord[lessLength - i]) {
+//         console.log("é palindroma!");
+//     } else {
+//         console.log(("non è palindroma"));
+//     }
+// }
+
+
+function wordIsAPalindrome (wordToCheck) {
+    let isPalindrome = false;
+    let lessLength = wordToCheck.length - 1;
+    for (let i = 0; i < wordToCheck; i++) {
+        if (wordToCheck[i] === wordToCheck[lessLength - i]) {
+            isPalindrome = true;
+        } else {
+            isPalindrome = false;
+        }
     }
+    return isPalindrome;
 }
