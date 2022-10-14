8a42209e05ab7879c3b5da4959d1808846b11b3b
scurenza <fedechiar@hotmail.it>
2022-10-14 17:48:07 +0200

    problemi con NaN

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -4,3 +4,20 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+// Faccio scegliere all'utente tra pari e dispari
+let userChoose = prompt("Scegli tra pari o dispari");
+while (userChoose !== 'pari' && userChoose !== 'dispari') {
+    userChoose = prompt("Ti ho detto di scegliere tra pari o dispari!");
+}
+console.log(userChoose);
+
+let userNumber = parseInt(prompt("Scegli un numero tra 1 e 5"));
+if (userNumber === NaN){
+    userNumber = parseInt(prompt("Ti ho detto di scrivere un numero!"))
+}
+
+// if (userNumber != NaN || userNumber > 5 && userNumber < 1) {
+//     userNumber = parseInt(prompt("Sei pregato di scegliere un numero valido"));
+// }
+console.log(userNumber);
